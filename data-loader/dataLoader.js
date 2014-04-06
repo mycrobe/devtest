9f5665a0fc355a40d966ac871d05224abcd7987d
mycrobe <git@mycro.be>
2014-04-05 17:00:04 -0700

    Load customers

--- data-loader/dataLoader.js
+++ data-loader/dataLoader.js
@@ -1,12 +1,34 @@
 var Client = require('node-rest-client').Client;
 var client = new Client();
+var fs = require('fs');
+var csv = require('csv');
 
 client.registerMethod("persistPerson", "http://localhost:3000/people", "POST");
 
-client.methods.persistPerson({data: {name:'Bar', email:'baz@foo.com'}, headers:{"Content-Type": "application/json"}},
-    function(data, response) {
-    // parsed response body as js object
-    console.log(data);
-    // raw response
-    console.log(response);
+// client errors; looks like we need to manually handle server errors, etc
+client.on('error', function (err) {
+    console.error('Something went wrong on the client', err);
 });
+
+function csvToRest(entityType, filename, restMethod, transform) {
+    csv().from.path(filename, {delimiter: '\t', columns: true})
+        .transform(transform)
+        .on('record', function (row, index) {
+            restMethod({data: row, headers: {"Content-Type": "application/json"}},
+                function (data, response) {
+                    if (response.statusCode != 200) {
+                        console.log('Error! %s returned with code %s. Error message %s \n   Data in question: %s', response.statusCode, data.code, data.message, JSON.stringify(row));
+                    }
+                    else {
+                        console.log("Added %s %s with id %s", entityType, row.name, data.json.id);
+                    }
+                });
+        });
+}
+
+csvToRest('person', '../data/customers.txt', client.methods.persistPerson, function (row) {
+    row.state_or_province = row['state/province'];
+    delete row['state/province'];
+    return row;
+});
+
